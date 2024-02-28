b221fcf1bcfec934ebbf4d919fa23d4aa38d1514
DaironRv <dairon.rodriguezvega@gmail.com>
2024-02-28 18:15:12 -0500

    Ajustes

--- monedas contorno/contornos.py
+++ monedas contorno/contornos.py
@@ -3,8 +3,8 @@ imagenn = cv2.imread("img1.png")
 grices= cv2.cvtColor(imagenn,cv2.COLOR_BGR2GRAY)
 
 # mostrar: 
-cv2.imshow("Está es tu imagen: ",grices)
-cv2.
+cv2.imshow("Imagen color: ",imagenn)
+cv2.imshow("imagen blanco y negro: ", grices)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
